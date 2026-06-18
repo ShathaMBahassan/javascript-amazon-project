589d31592b5aa2f3b485ec569141c185b716e5fa
ShathaMBahassan <92083449+Shathamhb@users.noreply.github.com>
2026-06-18 17:24:55 +0300

    load products

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,51 +1,5 @@
 console.log("Load js");
 
-
-// Products Data 
-const products = 
-    [
-        {
-            image: 'athletic-cotton-socks-6-pairs.jpg' ,
-            name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-            priceCents: 1090,
-            rating: 
-            {
-                stars: 4.5,
-                count: 87
-            }
-        }, 
-        {
-            image: 'intermediate-composite-basketball.jpg',
-            name: 'Intermediate Size Basketball',
-            priceCents: 2095,
-            rating:
-                {
-                    stars: 4,
-                    count: 127 
-                }
-        },
-        {
-            image:'adults-plain-cotton-tshirt-2-pack-teal.jpg',
-            name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-            priceCents: 799,
-            rating: 
-                {
-                    stars: 4.5,
-                    count: 56
-                } 
-        },
-        {
-            image:'black-2-slot-toaster.jpg',
-                name: '2 Slot Toaster - Black',
-                priceCents: 1899,
-                rating: 
-                    {
-                        stars: 5,
-                        count: 2197
-                    }   
-        }
-    ];
-
 let product_html = '';
 
 
