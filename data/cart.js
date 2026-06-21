3d79c3a957da2bc577e959d6d48eefd34e5e7097
ShathaMBahassan <92083449+Shathamhb@users.noreply.github.com>
2026-06-21 07:33:09 +0300

    Generate HTML for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,12 @@
 console.log("load cart");
-export const cart = [];
+export const cart = [{
+    id:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+},
+{
+    id:'901eb2ca-386d-432e-82f0-6fb1ee7bf969',
+    quantity: 4
+}];
 
 export function addToCart(product_id){
     let matchingItem;
